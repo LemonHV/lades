d38a4491efc6195a03d8ea3f60a366341b5ccace
vu.ma <matruongvu1@gmail.com>
2025-12-24 15:05:54 +0700

    fix: config auto redirect url

--- src/lades/settings.py
+++ src/lades/settings.py
@@ -183,6 +183,8 @@ EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
+APPEND_SLASH = False
+
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 AUTH_USER_MODEL = "account.User"
