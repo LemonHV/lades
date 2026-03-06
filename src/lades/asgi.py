e4b1a6d86fdb9725bae8688b8ca512a2cee48066
vu.ma <matruongvu1@gmail.com>
2026-03-06 21:11:08 +0700

    feat: fix asgi for websocket

--- src/lades/asgi.py
+++ src/lades/asgi.py
@@ -8,18 +8,20 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-import chat.routing
-import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lades.settings")
-django.setup()
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing
+
+django_asgi_app = get_asgi_application()
+
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)),
     }
 )
