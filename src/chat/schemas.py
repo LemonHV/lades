62d1217f44153e28b4edad59c67dd4e9966c7b1e
vu.ma <matruongvu1@gmail.com>
2026-03-17 02:41:45 +0700

    fix: fix schema of conversation

--- src/chat/schemas.py
+++ src/chat/schemas.py
@@ -1,5 +1,5 @@
 from ninja import ModelSchema, Schema
-from chat.models import Message, Notification
+from chat.models import Message, Notification, Conversation
 
 
 class MessageSchema(ModelSchema):
@@ -42,32 +42,12 @@ class UploadImageResponseSchema(Schema):
 
 
 class ConversationSchema(ModelSchema):
-    user_uid: str
-    user_name: str
-    last_message_content: str | None
-    last_message_type: str | None
-    last_message_at: str | None
-
     class Meta:
-        model = Message
-        fields = []
-
-    @staticmethod
-    def resolve_user_uid(obj):
-        return str(obj.user.uid)
-
-    @staticmethod
-    def resolve_user_name(obj):
-        return obj.user.name
-
-    @staticmethod
-    def resolve_last_message_content(obj):
-        return obj.last_message.content if obj.last_message else None
-
-    @staticmethod
-    def resolve_last_message_type(obj):
-        return obj.last_message.type if obj.last_message else None
-
-    @staticmethod
-    def resolve_last_message_at(obj):
-        return obj.last_message_at.isoformat() if obj.last_message_at else None
+        model = Conversation
+        fields = [
+            "uid",
+            "user",
+            "last_message",
+            "last_message_at",
+            "created_at",
+        ]
