e7fd353fab3ba7f73ac3756b783eb6470e57433b
vu.ma <matruongvu1@gmail.com>
2026-04-03 14:32:29 +0700

    fix(cart): fix response get cart item

--- src/cart/orm/cart.py
+++ src/cart/orm/cart.py
@@ -10,7 +10,6 @@ from product.orm.product import ProductORM
 
 
 class CartORM:
-    
     @staticmethod
     def get_or_create_cart(user: User) -> Cart:
         try:
@@ -19,7 +18,7 @@ class CartORM:
                 return cart
         except IntegrityError:
             return Cart.objects.get(user=user)
-            
+
     @staticmethod
     def add_item_to_cart(cart: Cart, payload: CartItemRequestSchema) -> CartItem:
         if payload.quantity <= 0:
@@ -41,9 +40,11 @@ class CartORM:
             cart_item.save(update_fields=["quantity", "price"])
 
         return cart_item
-    
+
     @staticmethod
-    def update_item_quantity(cart: Cart, cart_item_uid, quantity: int) -> CartItem | None:
+    def update_item_quantity(
+        cart: Cart, cart_item_uid, quantity: int
+    ) -> CartItem | None:
         cart_item = CartItem.objects.filter(
             uid=cart_item_uid,
             cart=cart,
@@ -56,7 +57,7 @@ class CartORM:
         cart_item.quantity = quantity
         cart_item.save(update_fields=["quantity"])
         return cart_item
-    
+
     @staticmethod
     def delete_cart_item(cart: Cart, cart_item_uid) -> bool:
         deleted_count, _ = CartItem.objects.filter(
@@ -64,22 +65,21 @@ class CartORM:
             cart=cart,
         ).delete()
         return deleted_count > 0
-    
+
     @staticmethod
     def clear_cart(cart: Cart) -> None:
         CartItem.objects.filter(cart=cart).delete()
-    
+
     @staticmethod
     def get_cart_items(cart: Cart) -> QuerySet[CartItem]:
         return (
             CartItem.objects.filter(cart=cart)
-            .select_related("product")
+            .select_related("product", "product__brand")
             .prefetch_related(
                 Prefetch(
                     "product__images",
-                    queryset=ProductImage.objects.all(),
+                    queryset=ProductImage.objects.select_related("attachment"),
                     to_attr="images",
                 )
             )
         )
-    
